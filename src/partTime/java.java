196cf212db0d093cace91f741f00d0c2dbbf6750
kaur6907 <kaur6907@sheridancollege.ca>
2019-05-23 13:00:29 -0400

    commit

---
+++ src/partTime/java.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partTime;
+
+/**
+ *
+ * @author Owner
+ */
+public class java {
+
+    private int numClass;
+
+    /**
+     * Get the value of numClass
+     *
+     * @return the value of numClass
+     */
+    public int getNumClass() {
+        return numClass;
+    }
+
+    /**
+     * Set the value of numClass
+     *
+     * @param numClass new value of numClass
+     */
+    public void setNumClass(int numClass) {
+        this.numClass = numClass;
+    }
+
+    
+}
